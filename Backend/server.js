c50ebcf1648c059eb3a0744d67da301eb199bb45
Gireesh Sharma <gireeshsharma@Gireeshs-MacBook.local>
2026-09-06 07:47:11 +0530

    Add React frontend and connect CheckMate AI chat with backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import getGeminiAPIResponse from "./utils/gemini.js";
+import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(express.json());
 app.use(cors());
+app.use("/api", chatRoutes);
 
 app.post("/test", async (req, res) => {
   try {
@@ -30,6 +33,20 @@ app.post("/test", async (req, res) => {
     });
   }
 });
+
+const connectDB = async () => {
+  try {
+    console.log("Mongo URI loaded:", !!process.env.MONGODB_URI);
+
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("Connected with Database!");
+  } catch (err) {
+    console.log("Failed to connect with DB", err);
+  }
+};
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
